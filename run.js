03660747f1149c03bf8014389cc06b330222f5ae
Andy Yaco-Mink <ayacomink@etsy.com>
2017-06-13 08:51:59 -0400

    Feeder updates

--- run.js
+++ run.js
@@ -1,65 +1,81 @@
-let lightblue = require('../src/lightblue')
-let common = require('../src/cli/commands/common')
+let lightblue = require('bean-sdk/src/lightblue')
+let common = require('bean-sdk/src/cli/commands/common')
+let util = require('util');
 const buffer = require('buffer')
 var moment = require('moment');
 
 let sdk = lightblue.sdk();
 
+opt = require('node-getopt').create([
+  [''  , 'dry'                , 'Dry run, do not feed or connect'],
+  // ['S' , 'short-with-arg=ARG'  , 'option with argument'],
+  ['M' , 'max=ARG'   , 'Max cycle length, default 60'],
+  ['m' , 'min=ARG'   , 'Min cycle length, default 5'],
+  ['c' , 'cycles=ARG'   , 'Cycles, default 150'],
+  // [''  , 'color[=COLOR]'       , 'COLOR is optional'],
+  // ['m' , 'multi-with-arg=ARG+' , 'multiple option with argument'],
+  // [''  , 'no-comment'],
+  // ['h' , 'help'                , 'display this help'],
+  // ['v' , 'version'             , 'show version']
+])              // create Getopt instance
+.bindHelp()     // bind option 'help' to default action
+.parseSystem(); // parse command line
+
 let data = 'CMD-FEED';
 
 let interval = 5000;
 let feed_length = 2000;
-let cycles = 10;
 
-var max_seconds = 60;
-var min_seconds = 5;
+let cycles = opt.options.cycles || 150;
+let max_seconds = opt.options.max || 60;
+let min_seconds = opt.options.min || 5;
+let start_time = (new Date()).getTime();
 
 // Ascii
 let buf = new buffer.Buffer(data, 'ascii')
 let device = null;
 var ran = 0;
 var errors = [];
+var promises = [];
+var elapsed = 0;
 
-async function feed_cycles() {
-  var elapsed = 0;
+function feed_cycles() {
   for (var i = 0; i < cycles; i++) {
     interval = Math.round((i / cycles) * (max_seconds-min_seconds) * 1000);
     interval -= Math.round(interval * Math.random() * 0.25);
     interval += min_seconds * 1000;
     elapsed += interval;
-    await new Promise((resolve, reject) => {
-      console.log("Sleeping for " + interval);
-      setTimeout( () => {
-        console.log("Feeding");
-        device.sendSerial(buf, (err) => {
-          if (err) {
-            console.log(err);
-            errors.push(err);
-          }
-          resolve();
-        });
-        if (i == cycles-1) {
-            commandComplete(null,i+1,elapsed);
-        }
-      }, interval );
-    });
+    promises[i] = new Promise(promise_to_feed);
   }
+  console.log(util.format("Feeding %s cyles %s", cycles, moment.duration(elapsed).humanize(true)));
+  Promise.all(promises).then(commandComplete);
 }
 
-var feed = function(device) {
-  feed_cycles();
-  // device.sendSerial(buf, (err) => feed_cycles());
+function promise_to_feed(resolve, reject) {
+  setTimeout( () => {
+    if (opt.options.dry) {
+      console.log('RESOLVING AFTER ' + moment.duration((new Date()).getTime() - start_time).seconds());
+      resolve();
+    } else {
+      feed(resolve);
+    }
+  }, elapsed );
 }
 
-function commandComplete(error,cycles,elapsed) {
-  console.log('')
-  if (error) {
-    console.log(`Command completed with error(s): ${error}`)
-    quit(1)
-  } else {
+
+var feed = function(resolve) {
+  device.sendSerial(buf, (err) => {
+    if (err) {
+      console.log(err);
+      errors.push(err);
+    }
+    resolve();
+  });
+}
+
+function commandComplete() {
     console.log('Fed ' + cycles + ' cycles in ' + moment.duration(Math.round(elapsed/1000), 'seconds').humanize() + " with " + errors.length + " errors");
     quit(0)
-  }
 }
 
 
@@ -72,9 +88,12 @@ function quit(rc) {
   })
 }
 
-common.connectToDevice(sdk,
-                null,
-                '05d4ed5403d84995a17d69a480733be1',
-                (connected_device) => {device = connected_device; feed_cycles();},
-                commandComplete);
-// feed_cycles();
+if (opt.options.dry) {
+  feed_cycles();
+} else {
+  common.connectToDevice(sdk,
+                  null,
+                  '05d4ed5403d84995a17d69a480733be1',
+                  (connected_device) => {device = connected_device; feed_cycles();},
+                  commandComplete);
+}
