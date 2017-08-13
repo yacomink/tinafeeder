a0ac3fa87ff6e237962d36791cf566448a9e9a60
Andy Yaco-Mink <andy@buzzfeed.com>
2017-08-13 10:50:15 -0400

    modularize runner

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,9 @@
 let lightblue = require('bean-sdk/src/lightblue');
 let common = require('bean-sdk/src/cli/commands/common');
 let util = require('util');
-const buffer = require('buffer');
-const sleep = require('sleep.async');
 var moment = require('moment');
-
 let sdk = lightblue.sdk();
+let Feeder = require('../lib/feeder');
 
 var opt = require('node-getopt').create([
   ['', 'dry', 'Dry run, do not feed or connect'],
@@ -22,94 +20,6 @@ var opt = require('node-getopt').create([
   ]
 ).bindHelp().parseSystem(); // parse command line
 
-let feed_length = 2000;
-const FIXED_TIME = 1;
-const FIXED_CYCLES = 2;
-
-let cycles = opt.options.cycles || 150;
-let max_seconds = opt.options.max || 60;
-let min_seconds = opt.options.min || 5;
-let interval = opt.options.interval || 5;
-let run_time = opt.options.run_time || 5 * 60;
-let start_time = new Date().getTime();
-
-let run_type = opt.options.run_time ? FIXED_TIME : FIXED_CYCLES;
-
-// Ascii
-let device = null;
-var ran = 0;
-var errors = [];
-var promises = [];
-var elapsed = 0;
-var rewind_cycles = 0;
-
-function calculate_fixed_cycles_intervals() {
-
-    var intervals = [];
-    for (var i = 0; i < cycles; i++) {
-      let interval = Math.round((i - rewind_cycles) / cycles * (max_seconds - min_seconds) * 1000);
-      interval += min_seconds * 1000;
-      interval -= Math.round(interval * Math.random() * 0.30);
-      if (opt.options.reset && rewind_cycles === 0 &&
-          (elapsed + interval >= (opt.options.reset * 1000))) {
-          // Just crossed the reset boundary, the next cycle will start over at min_seconds
-          rewind_cycles = i;
-      }
-      intervals.push(interval);
-      elapsed += interval;
-    }
-    // console.log(intervals.join("\n"));
-    return intervals;
-
-}
-
-function calculate_fixed_time_intervals() {
-
-    var intervals = [];
-    var elapsed = 0;
-    while (elapsed < opt.options.run_time) {
-      intervals.push(opt.options.interval * 1000);
-      elapsed += opt.options.interval*1;
-    }
-    return intervals;
-
-}
-
-async function feed_cycles() {
-    var intervals = (run_type == FIXED_TIME) ? 
-                    calculate_fixed_time_intervals() :
-                    calculate_fixed_cycles_intervals();
-
-    var total_time = intervals.reduce((carry, val) => carry*1 + val*1);
-
-    console.log("Feeding %s cyles in %sh %sm", 
-                intervals.length,
-                moment.duration(total_time).get('hours'),
-                moment.duration(total_time).get('minutes'));
-
-    for (var i = 0; i < intervals.length; i++) {
-      await sleep(intervals[i], !opt.options.dry ? feed : null);
-      if (!opt.options.dry) {
-        feed();        
-      }
-      console.log('%s Fed iteration %d after %s second delay',
-                  moment().format(),
-                  i + 1,
-                  moment.duration(intervals[i]).seconds());
-    }
-
-    commandComplete();
-}
-
-var feed = function () {
-  let buf = new buffer.Buffer('CMD-FEED', 'ascii');
-  device.sendSerial(buf, err => {
-    if (err) {
-      console.log(err);
-      errors.push(err);
-    }
-  });
-};
 
 function commandComplete() {
   console.log("Fed %d cycles in %s with %d errors",
@@ -133,12 +43,13 @@ function quit(rc) {
 
 
 if (opt.options.dry) {
-  feed_cycles();
+  var feeder = new Feeder(null, opt.options);
+  feeder.feed_cycles();
 } else {
-  common.connectToDevice(sdk, null, process.env.BEAN_ADDRESS, connected_device => {
+  common.connectToDevice(sdk, 'PetTutor Feeder', process.env.BEAN_ADDRESS, connected_device => {
     console.log('Connected to device');
-    device = connected_device;
-    feed_cycles();
+    var feeder = new Feeder(connected_device, opt.options);
+    feeder.feed_cycles();
   }, commandComplete);
 }
 
--- bin/server.js
+++ bin/server.js
@@ -2,87 +2,63 @@ var express = require('express');
 var app = express();
 var blink;
 
-const Blink = require('node-blink-security');
 let lightblue = require('bean-sdk/src/lightblue');
 let common = require('bean-sdk/src/cli/commands/common');
 let sdk = lightblue.sdk();
 const sleep = require('sleep.async');
 const child_process = require('child_process');
+let feeder_device = null;
 
-function cameraAction(res, fn) {
-    try {
-      if (!blink) {
-          var fresh_blink = new Blink(process.env.BLINK_EMAIL, process.env.BLINK_PASSWORD);
-          fresh_blink.setupSystem(process.env.BLINK_SYSTEM)
-            .then(() => {
-              blink = fresh_blink;
-              fn(res);
-            }, (error) => {
-              console.log(error);
-            });
-      } else {
-          fn(res);
-      }
-    } catch (ex) {
-      res.send(ex);
-    }
-}
-
-function serveSnap(res) {
-    var starting_thumb = blink.cameras.study.thumbnail;
-    blink.cameras.study
-        .imageRefresh()
-        .then(() => { return blink.cameras.study.fetchImageData(); })
-        .then((body) => {
-            res.writeHead(200, {'Content-Type': 'image/jpeg' });
-            res.end(body, 'binary');      
-        });
-}
-function takeSnap(res) {
-    blink.cameras.study.snapPicture().then((body) => {
-        res.send(body);      
-    });
-}
-
-async function takeAndServeSnap(res) {
-    var starting_thumb = await blink.cameras.study.imageRefresh();
-    await blink.cameras.study.snapPicture();
-    while (starting_thumb === await blink.cameras.study.imageRefresh()) {
-      await sleep(1000);
-    }
-    serveSnap(res);
-}
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/latest-snap', function (req, res) {
-    cameraAction(res, serveSnap);
+app.get('/_connect', function (req, res) {
+  common.connectToDevice(sdk, 'PetTutor Feeder', process.env.BEAN_ADDRESS, connected_device => {
+    feeder_device = connected_device;
+    res.send('Connected');
+  }, (err) => {
+    res.send(err);
+  } );
 });
 
-app.get('/snap', function (req, res) {
-    res.sendfile('./html/index.html');
+app.get('/_disconnect', function (req, res) {
+  if (feeder_device && feeder_device.isConnected()) {
+    feeder_device.disconnect((err) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send('Disconnected');
+      }
+    });
+  }
 });
 
-app.get('/_snap', function (req, res) {
-    cameraAction(res, takeAndServeSnap);
+app.get('/status', function (req, res) {
+  if (!feeder_device) {
+    res.send({'status': 'Not connected'});
+  } else if (feeder_device.isConnected()) {
+    res.send(feeder_device.serialize());
+  } else if (feeder_device.isConnectedorConnecting()) {
+    res.send({'status': 'Not connected'});
+  }
 });
+
 
 app.get('/_spawn', function(req, res) {
   var child = child_process.spawn('node',
-        ['-r', 'babel-polyfill', './build/bin/run.js', '--min=12', 
-        '--max=23', '--reset=600',
-        '--cycles=125' ],
+        ['-r', 'babel-polyfill', './build/bin/run.js', '--min=10', 
+        '--max=20', 
+        '--cycles=100' ],
         { cwd: process.cwd, detached: true, stdio: 'inherit' });
   child.unref();
   res.send();
 });
 
+console.log(process.env.PORT);
 var server = app.listen(process.env.PORT, function () {
-
   var port = server.address().port;
   console.log('Listening on port ', port);
-
 });
